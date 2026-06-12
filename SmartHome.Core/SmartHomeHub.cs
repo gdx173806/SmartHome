f229960b8c6859de4fffe832c8d8cb0d086d1425
dawid <gdx173806@student.gdansk.merito.pl>
2026-06-12 15:05:50 +0200

    added central devices hub

---
+++ SmartHome.Core/SmartHomeHub.cs
@@ -0,0 +1,62 @@
+namespace SmartHome.Core;
+
+public class SmartHomeHub
+{
+    private readonly List<IDevice> _devices = new List<IDevice>();
+
+    public string Name { get; }
+
+    public SmartHomeHub(string name)
+    {
+        if (string.IsNullOrEmpty(name))
+        {
+            throw new ArgumentException("Hub name cannot be empty!");
+        }
+        Name = name;
+    }
+    
+    public List<IDevice> GetDevices()
+    {
+        return _devices;
+    }
+
+
+    public static SmartHomeHub operator +(SmartHomeHub hub, IDevice? device)
+    {
+        if (device == null) return hub;
+
+        // sprawdzenie czy urzadzenie o takim ID jest juz na liscie
+        foreach (var d in hub._devices)
+        {
+            if (d.Id == device.Id)
+            {
+                throw new ArgumentException("Device with this ID already exists!");
+            }
+        }
+
+        hub._devices.Add(device);
+        return hub;
+    }
+    
+    // wyszukiwanie po ID
+    public IDevice? this[string id]
+    {
+        get
+        {
+            // LINQ do przeszukania listy
+            return _devices.FirstOrDefault(d => d.Id == id);
+        }
+    }
+
+    // LINQ - filtrowanie po room
+    public List<IDevice> GetDevicesInRoom(string room)
+    {
+        return _devices.Where(d => d.Room == room).ToList();
+    }
+
+    // LINQ - filtrowanie tylko wlaczonych urzadzen
+    public List<IDevice> GetActiveDevices()
+    {
+        return _devices.Where(d => d.IsEnabled).ToList();
+    }
+}
